8e2fba59d0c0289f9b431e5894aee3b14c2aebe9
Adam Perkins <ap@Adams-MacBook-Pro.local>
2017-02-12 15:37:34 -0500

    Hello World project with instructions on how to setup a basic Eclipse environment and OpenOCD.

---
+++ hello_world/Debug/Src/stm32f4xx_hal_msp.d
@@ -0,0 +1,76 @@
+Src/stm32f4xx_hal_msp.o: ../Src/stm32f4xx_hal_msp.c \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/ap/git/stm32/hello_world/Inc/stm32f4xx_hal_conf.h \
+ /Users/ap/git/stm32/hello_world/Inc/main.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cmInstr.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cmFunc.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cmSimd.h \
+ /Users/ap/git/stm32/hello_world/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/ap/git/stm32/hello_world/Inc/stm32f4xx_hal_conf.h:
+
+/Users/ap/git/stm32/hello_world/Inc/main.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/ap/git/stm32/hello_world/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
